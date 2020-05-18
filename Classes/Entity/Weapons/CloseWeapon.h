f0dc4f44b3defc84bbff5b06ba1f94e348a81d26
ZhichenRen <1337795211@qq.com>
2020-05-18 22:42:41 +0800

    Close Range Weapon
    Add close range weapon;

---
+++ Classes/Entity/Weapons/CloseWeapon.h
@@ -0,0 +1,25 @@
+#ifndef __CloseWeapon_H__
+#define __CloseWeapon_H__
+
+#include "Entity\Weapons\Weapon.h"
+#define RIGHT 1
+#define LEFT 2
+
+class CloseWeapon :public Weapon
+{
+public:
+	virtual bool init();
+	CREATE_FUNC(CloseWeapon);
+public:
+	virtual void attack(Point pos);
+	bool isCollideWith(Entity* entity);
+protected:
+	float m_attack_speed;
+	int m_damage;
+private:
+	bool m_is_attack;
+	int m_attack_position;
+};
+
+
+#endif
