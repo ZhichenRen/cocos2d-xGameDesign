ecd34f458d58c63f699bd0315dc7e25ce4037dde
ZhichenRen <1337795211@qq.com>
2020-05-23 15:27:58 +0800

    Basic PauseScene Class
    Basic function finished;

--- Classes/Scene/PauseScene.h
+++ Classes/Scene/PauseScene.h
@@ -2,11 +2,15 @@
 #define __PauseScene_H__
 
 #include "cocos2d.h"
+
 using namespace cocos2d;
 
 class PauseScene :public Layer
 {
-
+public:
+	virtual bool init();
+	static Scene* createScene(CCRenderTexture* background);
+	CREATE_FUNC(PauseScene);
 };
 
 #endif
