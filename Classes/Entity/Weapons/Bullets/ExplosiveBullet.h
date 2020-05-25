0e532428b1c3ccc693e36fe02f038566e8d99cca
ZhichenRen <1337795211@qq.com>
2020-05-25 21:53:07 +0800

    Add Explosive Bullet Class
    Animation complete;

---
+++ Classes/Entity/Weapons/Bullets/ExplosiveBullet.h
@@ -0,0 +1,19 @@
+#ifndef __ExplosiveBullet_H__
+#define __ExplosiveBullet_H__
+
+#include "Entity\Weapons\Bullets\Bullet.h"
+
+class ExplosiveBullet :public Bullet
+{
+public:
+	CREATE_FUNC(ExplosiveBullet);
+	virtual bool init();
+	void explode();
+	int getExplosionDamage()const;
+	void setInfo(int range, int damage, int explosion_range, int explosion_damage);
+private:
+	int m_explosion_damage;
+	int m_explosion_range;
+};
+
+#endif
