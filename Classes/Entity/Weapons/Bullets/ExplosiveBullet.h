868545515ec30002cc918559f8d8989fc7a61fed
ZhichenRen <1337795211@qq.com>
2020-05-26 20:32:26 +0800

    Update ExplosiveBullet.h

--- Classes/Entity/Weapons/Bullets/ExplosiveBullet.h
+++ Classes/Entity/Weapons/Bullets/ExplosiveBullet.h
@@ -10,6 +10,7 @@ public:
 	virtual bool init();
 	void explode();
 	int getExplosionDamage()const;
+	int getExplosionRange()const;
 	void setInfo(int range, int damage, int explosion_range, int explosion_damage);
 private:
 	int m_explosion_damage;
