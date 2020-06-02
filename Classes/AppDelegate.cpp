87f40feac0b76e7719d58b8947876897512037cf
BigPearrrr <1029347073@qq.com>
2020-06-02 20:31:18 +0800

    fix conflict

--- Classes/AppDelegate.cpp
+++ Classes/AppDelegate.cpp
@@ -96,7 +96,7 @@ bool AppDelegate::applicationDidFinishLaunching() {
     register_all_packages();
 
     // create a scene. it's an autorelease object
-    auto scene = TollgateScene::createScene();
+    auto scene = AdventureMapLayer::createScene();
 
     // run
     director->runWithScene(scene);
