23a92b192888a39f8106c8c6cb2d769f17c0adaa
ZhichenRen <1337795211@qq.com>
2020-06-13 21:23:24 +0800

    Update GoldenSword.cpp

--- Classes/Entity/Weapons/GoldenSword.cpp
+++ Classes/Entity/Weapons/GoldenSword.cpp
@@ -6,7 +6,7 @@ bool GoldenSword::init()
 	m_range = 25;
 	m_attack_speed = 0.2f;
 	m_crit_rate = 0.2f;
-	bindSprite(Sprite::create("GoldenSword!.png"), 0.16f, 0.16f);
+	bindSprite(Sprite::create("GoldenSword!.png"), 0.2f, 0.2f);
 	m_sprite->setAnchorPoint(Vec2(0, 0));
 	return true;
 }
