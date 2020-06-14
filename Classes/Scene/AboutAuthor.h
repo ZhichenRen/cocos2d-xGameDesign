1c3da37b50875c66b1be689f88e5463725ab3ce4
ZhichenRen <1337795211@qq.com>
2020-06-14 12:58:58 +0800

    add DeathScene AboutAuthor and Resource

---
+++ Classes/Scene/AboutAuthor.h
@@ -0,0 +1,19 @@
+﻿#ifndef __AboutAuthor_H__
+#define __AboutAuthor_H__
+#include "cocos2d.h"
+#include "editor-support/cocostudio/CCSGUIReader.h"
+#include "ui/CocosGUI.h"
+using namespace cocos2d::ui;
+using namespace cocos2d;
+
+class AboutAuthor :public Layer
+{
+public:
+	virtual bool init();
+	CREATE_FUNC(AboutAuthor);
+private:
+	void loadUI();
+	void back(Ref*, TouchEventType type);
+};
+
+#endif
