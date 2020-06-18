124a1a7cde131c6852be816d78a6aa1452a641a3
ZhichenRen <1337795211@qq.com>
2020-06-18 22:05:15 +0800

    add stone sword
    basic weapon

---
+++ Classes/Entity/Weapons/StoneSword.cpp
@@ -0,0 +1,33 @@
+﻿#include "Entity/Weapons/StoneSword.h"
+
+bool StoneSword::init()
+{
+	m_damage = 8;
+	m_range = 25;
+	m_attack_speed = 0.18f;
+	m_crit_rate = 0.05f;
+	m_weaponName = "StoneSword";
+	bindSprite(Sprite::create("StoneSword.png"), 0.16f, 0.16f);
+	m_sprite->setAnchorPoint(Vec2(0, 0));
+	return true;
+}
+
+void StoneSword::upgrade()
+{
+	m_damage = 15;
+	m_range = 30;
+	m_attack_speed = 0.15f;
+	m_crit_rate = 0.1f;
+	bindSprite(Sprite::create("GoldenSword!.png"), 0.18f, 0.18f);
+	auto upgrade_effect = ParticleFlower::create();
+	upgrade_effect->setEmitterMode(ParticleSystem::Mode::RADIUS);
+	upgrade_effect->setPositionType(ParticleSystem::PositionType::RELATIVE);
+	upgrade_effect->setAutoRemoveOnFinish(true);
+	upgrade_effect->setTotalParticles(100);
+	upgrade_effect->setPosition(getSprite()->getContentSize().width / 2, getSprite()->getContentSize().height / 2);
+	upgrade_effect->setDuration(-1);
+	upgrade_effect->setStartRadius(50.0f);
+	upgrade_effect->setStartRadiusVar(25.0f);
+	upgrade_effect->setEndRadius(50.0f);
+	getSprite()->addChild(upgrade_effect);
+}
