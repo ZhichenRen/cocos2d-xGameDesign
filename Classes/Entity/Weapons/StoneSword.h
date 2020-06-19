e211eb52bf11d8add994a300508910f6ae41c585
ZhichenRen <1337795211@qq.com>
2020-06-19 19:04:55 +0800

    Update StoneSword.h

--- Classes/Entity/Weapons/StoneSword.h
+++ Classes/Entity/Weapons/StoneSword.h
@@ -1,1 +1,14 @@
-#pragma once
+﻿#ifndef __StoneSword_H__
+#define __StoneSword_H__
+
+#include "Entity/Weapons/CloseWeapon.h"
+
+class StoneSword :public CloseWeapon
+{
+public:
+	virtual bool init();
+	CREATE_FUNC(StoneSword);
+	virtual void upgrade();
+};
+
+#endif
